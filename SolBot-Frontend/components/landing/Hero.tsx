5a5bdaae9f09031d36e1c93cd62bc0bd33230c6e
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-08-11 01:03:47 +0530

    ticker mover

--- SolBot-Frontend/components/landing/Hero.tsx
+++ SolBot-Frontend/components/landing/Hero.tsx
@@ -1,34 +1,59 @@
+"use client";
 import React from "react";
 import Button from "../button/Button";
+import { motion } from "motion/react";
 
 function Hero() {
   return (
     <section className="py-24">
       <div className="container mx-auto">
-        <div className="flex justify-center">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.2 }}
+          className="flex justify-center"
+        >
           <div className="py-1 px-3 inline-flex bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
             ⚡ Tap. Trade. Done.
           </div>
-        </div>
-        <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6 mx-auto max-w-5xl">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.9 }}
+          className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6 mx-auto max-w-5xl"
+        >
           Crypto trading, simplified for everyone
-        </h1>
-        <p className="text-center text-xl text-white/50 mt-8 mx-auto max-w-3xl">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1.5 }}
+          className="text-center text-xl text-white/50 mt-8 mx-auto max-w-3xl"
+        >
           SolBot makes token trading effortless. From wallet creation to
           real-time swaps, everything happens fast — right from Discord.
-        </p>
-        
-
-        <form className=" flex border border-white/15 rounded-full p-2 mt-8  justify-between max-w-lg mx-auto">
-          <input
-            className="bg-transparent px-4 md:flex-1"
-            type="email"
-            placeholder="Enter your email"
-          ></input>
-          <Button variant="primary" type="submit" className="whitespace-normal">
-            Join the Beta
-          </Button>
-        </form>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 2.1 }}
+        >
+          <form className=" flex border border-white/15 rounded-full p-2 mt-8  justify-between max-w-lg mx-auto">
+            <input
+              className="bg-transparent px-4 md:flex-1"
+              type="email"
+              placeholder="Enter your email"
+            ></input>
+            <Button
+              variant="primary"
+              type="submit"
+              className="whitespace-normal"
+            >
+              Join the Beta
+            </Button>
+          </form>
+        </motion.div>
       </div>
     </section>
   );
--- SolBot-Frontend/components/landing/LogoTicker.tsx
+++ SolBot-Frontend/components/landing/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import alchemy from "@/public/alc.jpeg";
 import helius from "@/public/hel.jpeg";
@@ -6,7 +7,7 @@ import jupiter from "@/public/jup.png";
 import solana from "@/public/sol.png";
 import superTeam from "@/public/super.jpg";
 import Image from "next/image";
-
+import {motion } from "motion/react";
 const Logos = [
   {
     name: "Solana",
@@ -38,29 +39,42 @@ function LogoTicker() {
   return (
     <section className="py-20 overflow-x-clip">
       <div className="container mx-auto">
-        <h2 className="text-center text-2xl  text-white/80 font-medium">Powered by the Solana ecosystem</h2>
+        <h2 className="text-center text-2xl  text-white/80 font-medium">
+          Powered by the Solana ecosystem
+        </h2>
         <p className="text-center text-lg font-medium text-white/50 mt-8">
           Built using the best of Solana — fast, composable, and battle-tested
           tools.
         </p>
-        <div className="overflow-hidden mt-12 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex gap-24">
-            {Logos.map((logo) => (
-              <div className="flex items-center gap-2 pr-24">
-                <Image
-                  src={logo.image}
-                  key={logo.name}
-                  alt={logo.name}
-                  width={50}
-                  height={50}
-                  className="rounded-full object-cover"
-                ></Image>
-                <h3 className="text-white text-lg font-medium whitespace-nowrap">
-                  {logo.name}
-                </h3>
-              </div>
+        <div className="flex overflow-hidden mt-12 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <motion.div
+            animate={{ x: "-50%" }}
+            transition={{
+              duration:30,
+              repeat: Infinity,
+              ease: "linear",
+            }}
+          className="flex flex-none gap-24">
+            {Array.from({ length: 3 }).map((_, index) => (
+              <React.Fragment key={index}>
+                {Logos.map((logo) => (
+                  <div className="flex items-center gap-2 pr-24">
+                    <Image
+                      src={logo.image}
+                      key={logo.name}
+                      alt={logo.name}
+                      width={50}
+                      height={50}
+                      className="rounded-full object-cover"
+                    ></Image>
+                    <h3 className="text-white text-lg font-medium whitespace-nowrap">
+                      {logo.name}
+                    </h3>
+                  </div>
+                ))}
+              </React.Fragment>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
