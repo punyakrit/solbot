2395c50e084bb1c5ec0ad622ded1fd8551ee5561
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-08-11 02:00:50 +0530

    cta

--- SolBot-Frontend/components/landing/Cta.tsx
+++ SolBot-Frontend/components/landing/Cta.tsx
@@ -1,17 +1,23 @@
+
+"use client"
+import { motion } from "motion/react";
 import React from "react";
 
 function Cta() {
   return (
     <section className="py-24  text-center">
       <div className="overflow-x-clip p-4 flex">
-        <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
+        <motion.div
+          initial={{ x: "-50%" }}
+          transition={{ duration: 30, ease: "linear", repeat: Infinity }}
+        className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
           {Array.from({ length: 10 }, (_, i) => (
             <div key={i} className="flex items-center gap-16">
               <span className="text-lime-400 text-7xl">&#10038;</span>
               <span>Try it for free</span>
             </div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
