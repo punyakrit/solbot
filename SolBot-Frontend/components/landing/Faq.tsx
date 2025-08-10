5abf28757945c824f4ced8fa7b147578b8744170
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-08-11 01:55:41 +0530

    faqs

--- SolBot-Frontend/components/landing/Faq.tsx
+++ SolBot-Frontend/components/landing/Faq.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { AnimatePresence, motion } from "motion/react";
 import React from "react";
 
 export const faqs = [
@@ -29,7 +31,7 @@ export const faqs = [
 ];
 
 function Faq() {
-  const selectedIndex = 0;
+  const [selectedIndex, setSelectedIndex] = React.useState<number>(0);
   return (
     <section className="py-24">
       <div className="container mx-auto">
@@ -47,11 +49,14 @@ function Faq() {
               key={faq.question}
               className="bg-neutral-900 rounded-2xl border border-white/10 p-6"
             >
-              <div className="flex items-center justify-between">
+              <div
+                className="flex items-center justify-between cursor-pointer"
+                onClick={() => setSelectedIndex(faqIndex)}
+              >
                 <div className="font-medium">{faq.question}</div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className={`w-5 h-5 text-lime-400 flex-shrink-0 ${
+                  className={`w-5 h-5 text-lime-400 flex-shrink-0 transition duration-300 ${
                     selectedIndex === faqIndex ? "rotate-45" : ""
                   }`}
                   fill="none"
@@ -66,13 +71,20 @@ function Faq() {
                   />
                 </svg>
               </div>
-              <div
-                className={`mt-6 ${
-                  selectedIndex === faqIndex ? "block" : "hidden"
-                }`}
-              >
-                <div className="text-white/50">{faq.answer}</div>
-              </div>
+              <AnimatePresence>
+
+              {selectedIndex === faqIndex && (
+                <motion.div
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: "auto" }}
+                exit={{ opacity: 0, height: 0 }}
+                transition={{ duration: 0.3 }}
+                className={`mt-6 overflow-hidden`}
+                >
+                  <div className="text-white/50">{faq.answer}</div>
+                </motion.div>
+              )}
+              </AnimatePresence>
             </div>
           ))}
         </div>
